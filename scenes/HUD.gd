fa692e1bbac0f1c11b7dcb20e6cd142880893fd5
JohnathanLiu7799 <49369829+JohnathanLiu7799@users.noreply.github.com>
2022-01-30 12:44:10 -0500

    is it the built in fuck up

--- scenes/HUD.gd
+++ scenes/HUD.gd
@@ -9,14 +9,18 @@ func show_message(text):
 	$MessageTimer.start()
 
 func _ready():
+	on_round_start()
+	
+func on_round_start():
 	emit_signal('countdown_start')
 	for i in range (3,0,-1):
 		show_message(i)
 		yield($MessageTimer,'timeout')
 	show_message('DUEL')
 	yield($MessageTimer,'timeout')
-	emit_signal('countdown_end')
 	$Message.hide()
+	emit_signal('countdown_end')
+	
 
 #connect('request_clone', get_node('/root/SnapshotManager'), '_on_clone_requested')
 
