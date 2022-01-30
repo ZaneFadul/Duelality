ed3d47afeb105a48e1aa6aaae60aa444e5b0f423
NotLiu <aliu@lediur.com>
2022-01-30 01:26:12 -0600

    fix one way platforms, put map back in

--- scenes/controller/actions/Crouch.gd
+++ scenes/controller/actions/Crouch.gd
@@ -3,7 +3,9 @@ extends Node
 func function(params):
 	var player = params['player']
 	player._crouch_pressed = true
+	player.set_collision_mask_bit(1, false)
 
 func cleanup(params):
 	var player = params['player']
 	player._crouch_pressed = false
+	player.set_collision_mask_bit(1, true)
