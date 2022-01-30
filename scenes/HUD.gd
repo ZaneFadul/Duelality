68401765fe389919efc7281222e8c4366291b63a
JohnathanLiu7799 <49369829+JohnathanLiu7799@users.noreply.github.com>
2022-01-30 14:03:22 -0500

    freeze during countdown

--- scenes/HUD.gd
+++ scenes/HUD.gd
@@ -3,15 +3,20 @@ extends CanvasLayer
 signal countdown_start
 signal countdown_end
 
+func update_round(new_round):
+	$Round.text = str(new_round)
+	$Round.show()
+
 func show_message(text):
 	$Message.text = str(text)
 	$Message.show()
 	$MessageTimer.start()
 
 func _ready():
-	on_round_start()
+	get_node('/root/SnapshotManager').connect('new_round',self , 'on_round_start')
 	
 func on_round_start():
+	update_round(get_node('/root/SnapshotManager').round_num)
 	emit_signal('countdown_start')
 	for i in range (3,0,-1):
 		show_message(i)
