671c9d84d6f04dea16160eb0dd99db080148803e
ZaneFadul <theboonandthezane@gmail.com>
2022-01-30 12:12:22 -0500

    fix merge conflicts

--- scenes/managers/Game.gd
+++ scenes/managers/Game.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 signal request_clone(mainscene, player_scene, snapshot_payload)
 signal round_reset()
+signal game_started()
 
 var player1inputconfig = {
 	"LEFT": "MoveLeft",
@@ -38,6 +39,7 @@ onready var player_scene = preload("res://scenes/PlayerController.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	connect('request_clone', get_node('/root/SnapshotManager'), '_on_clone_requested')
+	connect('game_started', get_node('/root/AudioGlobals'), '_on_game_started')
 	sm = get_node('/root/SnapshotManager')
 	print(sm.round_snapshots)
 	var main_instance = main_scene.instance()
@@ -49,6 +51,7 @@ func _ready():
 	p2.position = p2_starting_pos
 	main_instance.add_players(p1, p2)
 	add_child(main_instance)
+	emit_signal('game_started')
 	#Recreate all clones
 	for i in range(sm.round_num):
 		emit_signal('request_clone', p1.filename, p1_starting_pos, sm.round_snapshots[sm.round_num]["p1"], $Main)
@@ -82,7 +85,7 @@ func goto_next_round(player, killed_player, mainscene):
 	p1.position = p1_starting_pos
 	p2.position = p2_starting_pos
 	p1.reset() #Start controller back at 0 seconds
-	p2.reset() 
+	p2.reset()
 	if sm.round_num == MAX_ROUNDS:
 		handle_win()
 	else:
