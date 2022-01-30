8627449ff8dae659308ec6cf0605235715a7b585
JohnathanLiu7799 <49369829+JohnathanLiu7799@users.noreply.github.com>
2022-01-30 16:43:40 -0500

    particle attempt

--- scenes/controller/snapshot/SnapshotManager.gd
+++ scenes/controller/snapshot/SnapshotManager.gd
@@ -2,6 +2,7 @@ extends Node
 var round_snapshots = {}
 var round_num = 0
 
+
 signal new_round()
 
 func incr_round_num():
@@ -11,7 +12,7 @@ func incr_round_num():
 func _on_clone_requested(player_file, player_pos, snapshot_payload, mainscene):
 	var new_player = load(player_file).instance()
 	new_player.position = player_pos
-	new_player.get_node('Sprite').set_material(null)
+	new_player.get_node('Sprite').modulate.a = .5
 	
 	var controllerPlayer = new_player.get_node("ControllerPlayer")
 	controllerPlayer.upload_payload(snapshot_payload)
